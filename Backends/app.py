8cee7d5bb79b7ffd090e38613b428319151a3584
rengganabakta <rengganabakta@gmail.com>
2025-01-21 10:48:39 +0700

    frontend update

--- Backends/app.py
+++ Backends/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import sqlite3
 
 # Inisialisasi aplikasi Flask
 app = Flask(__name__)
+CORS(app)
 
 # Fungsi untuk koneksi ke database SQLite
 def get_db_connection():
